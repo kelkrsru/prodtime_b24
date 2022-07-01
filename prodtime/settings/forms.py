9109ff29dacf172fd7d224d03eaf8d52e7e0dc6c
kelkrsru <kelkrsru@gmail.com>
2022-07-01 10:37:54 +0700

    Final equivalent

--- prodtime/settings/forms.py
+++ prodtime/settings/forms.py
@@ -3,10 +3,21 @@ from django import forms
 from .models import SettingsPortal
 
 
-class SettingsPortalForm(forms.ModelForm):
+class SettingsDealPortalForm(forms.ModelForm):
     """Форма Настройки портала"""
 
     class Meta:
         model = SettingsPortal
-        fields = ('create_deal', 'name_deal', 'create_task', 'name_task',
+        fields = ('create_deal', 'name_deal', 'category_id', 'stage_code',
+                  'real_deal_code', 'create_task', 'name_task',
                   'task_deadline',)
+
+
+class SettingsEquivalentPortalForm(forms.ModelForm):
+    """Форма Настройки портала"""
+
+    class Meta:
+        model = SettingsPortal
+        fields = ('section_list_id', 'default_section_id', 'real_section_code',
+                  'copy_section_code', 'equivalent_code',
+                  'sum_equivalent_code',)
